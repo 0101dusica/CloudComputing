f3531adf8678dab2d04e40519ede55fa523021c9
0101dusica <trbovicdusica@gmail.com>
2024-06-30 19:44:49 +0200

    Main Page Implemented

--- cloudMovie/src/app/app/layout/layout.module.ts
+++ cloudMovie/src/app/app/layout/layout.module.ts
@@ -1,15 +1,23 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
-
-
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
+    NavBarComponent,
+    FooterComponent,
     MainPageComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    NavBarComponent,
+    FooterComponent,
+    MainPageComponent
   ]
 })
+
 export class LayoutModule { }
