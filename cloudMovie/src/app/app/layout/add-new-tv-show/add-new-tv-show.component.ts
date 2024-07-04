426cb8a9ba476a9c90fa29435c5b1fdb67d40ea8
0101dusica <trbovicdusica@gmail.com>
2024-07-04 21:00:08 +0200

    add new tv show page implemented

--- cloudMovie/src/app/app/layout/add-new-tv-show/add-new-tv-show.component.ts
+++ cloudMovie/src/app/app/layout/add-new-tv-show/add-new-tv-show.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-new-tv-show.component.css']
 })
 export class AddNewTvShowComponent {
+  show = {
+    title: '',
+    year: 0,
+    description: '',
+    seasons: 0, 
+    actors: [] as string[],
+    directors: [] as string[],
+    genres: [] as string[]
+  };
 
+  genres = ['Action', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Romance', 'Sci-Fi', 'Thriller'];
+  actors = ['Actor 1', 'Actor 2', 'Actor 3', 'Actor 4'];
+  directors = ['Director 1', 'Director 2', 'Director 3', 'Director 4'];
+
+  onFileSelected(event: any, type: string) {
+    const file = event.target.files[0];
+    // Implement your file handling logic here
+  }
+
+  onSubmit() {
+    console.log('Show data:', this.show);
+    // Implement your submit logic here
+  }
+
+  onCheckboxChange(event: any, type: 'actors' | 'directors' | 'genres') {
+    const value = event.target.value;
+    if (event.target.checked) {
+      this.show[type].push(value);
+    } else {
+      const index = this.show[type].indexOf(value);
+      if (index !== -1) {
+        this.show[type].splice(index, 1);
+      }
+    }
+  }
 }
