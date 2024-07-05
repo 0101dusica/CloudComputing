19e36345c7a1f264c3c88488e301cfb35dc34711
Vesna Vasic <vasic.vesna.06@gmail.com>
2024-07-05 21:01:21 +0200

    Completed search

--- cloudMovie/src/app/app/layout/search/search.component.ts
+++ cloudMovie/src/app/app/layout/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MovieService} from "../movie.service";
 
 @Component({
   selector: 'app-search',
@@ -16,44 +17,18 @@ export class SearchComponent {
 
   genres = ['Action', 'Comedy', 'Drama', 'Horror', 'Sci-Fi', 'Romance'];
 
-  movies = [
-    { title: 'Apollo 13', image: '../../../../assets/poster-one.jpg', description: 'Space mission', actors: ['Tom Hanks'], directors: ['Ron Howard'], genres: ['Drama', 'History'] },
-    { title: 'Oldboy', image: '../../../../assets/poster-two.jpg', description: 'Revenge story', actors: ['Choi Min-sik'], directors: ['Park Chan-wook'], genres: ['Action', 'Thriller'] },
-    { title: 'Pacific Rim', image: '../../../../assets/poster-three.jpg', description: 'Robots vs Monsters', actors: ['Charlie Hunnam'], directors: ['Guillermo del Toro'], genres: ['Action', 'Sci-Fi'] },
-    { title: 'Jurassic World', image: '../../../../assets/poster-four.jpg', description: 'Dinosaur Park', actors: ['Chris Pratt'], directors: ['Colin Trevorrow'], genres: ['Action', 'Sci-Fi'] },
-    { title: 'The Garfield Show', image: '../../../../assets/poster-five.jpg', description: 'Cartoon show', actors: ['Frank Welker'], directors: ['Phil Roman'], genres: ['Comedy', 'Family'] },
-    { title: 'Blazing Saddles', image: '../../../../assets/poster-six.jpg', description: 'Western parody', actors: ['Cleavon Little'], directors: ['Mel Brooks'], genres: ['Comedy', 'Western'] },
-    { title: 'Apollo 13', image: '../../../../assets/poster-seven.jpg', description: 'Space mission', actors: ['Tom Hanks'], directors: ['Ron Howard'], genres: ['Drama', 'History'] },
-    { title: 'Oldboy', image: '../../../../assets/poster-eight.jpg', description: 'Revenge story', actors: ['Choi Min-sik'], directors: ['Park Chan-wook'], genres: ['Action', 'Thriller'] },
-    { title: 'Pacific Rim', image: '../../../../assets/poster-nine.jpg', description: 'Robots vs Monsters', actors: ['Charlie Hunnam'], directors: ['Guillermo del Toro'], genres: ['Action', 'Sci-Fi'] },
-    { title: 'Jurassic World', image: '../../../../assets/poster-ten.jpg', description: 'Dinosaur Park', actors: ['Chris Pratt'], directors: ['Colin Trevorrow'], genres: ['Action', 'Sci-Fi'] },
-    { title: 'The Garfield Show', image: '../../../../assets/poster-eleven.jpg', description: 'Cartoon show', actors: ['Frank Welker'], directors: ['Phil Roman'], genres: ['Comedy', 'Family'] },
-    { title: 'Blazing Saddles', image: '../../../../assets/poster-twelve.png', description: 'Western parody', actors: ['Cleavon Little'], directors: ['Mel Brooks'], genres: ['Comedy', 'Western'] },    { title: 'Apollo 13', image: '../../../../assets/poster-one.jpg', description: 'Space mission', actors: ['Tom Hanks'], directors: ['Ron Howard'], genres: ['Drama', 'History'] },
-    { title: 'Oldboy', image: '../../../../assets/poster-two.jpg', description: 'Revenge story', actors: ['Choi Min-sik'], directors: ['Park Chan-wook'], genres: ['Action', 'Thriller'] },
-    { title: 'Pacific Rim', image: '../../../../assets/poster-three.jpg', description: 'Robots vs Monsters', actors: ['Charlie Hunnam'], directors: ['Guillermo del Toro'], genres: ['Action', 'Sci-Fi'] },
-    { title: 'Jurassic World', image: '../../../../assets/poster-four.jpg', description: 'Dinosaur Park', actors: ['Chris Pratt'], directors: ['Colin Trevorrow'], genres: ['Action', 'Sci-Fi'] },
-    { title: 'The Garfield Show', image: '../../../../assets/poster-five.jpg', description: 'Cartoon show', actors: ['Frank Welker'], directors: ['Phil Roman'], genres: ['Comedy', 'Family'] },
-    { title: 'Blazing Saddles', image: '../../../../assets/poster-six.jpg', description: 'Western parody', actors: ['Cleavon Little'], directors: ['Mel Brooks'], genres: ['Comedy', 'Western'] },
-    { title: 'Apollo 13', image: '../../../../assets/poster-seven.jpg', description: 'Space mission', actors: ['Tom Hanks'], directors: ['Ron Howard'], genres: ['Drama', 'History'] },
-    { title: 'Oldboy', image: '../../../../assets/poster-eight.jpg', description: 'Revenge story', actors: ['Choi Min-sik'], directors: ['Park Chan-wook'], genres: ['Action', 'Thriller'] },
-    { title: 'Pacific Rim', image: '../../../../assets/poster-nine.jpg', description: 'Robots vs Monsters', actors: ['Charlie Hunnam'], directors: ['Guillermo del Toro'], genres: ['Action', 'Sci-Fi'] },
-    { title: 'Jurassic World', image: '../../../../assets/poster-ten.jpg', description: 'Dinosaur Park', actors: ['Chris Pratt'], directors: ['Colin Trevorrow'], genres: ['Action', 'Sci-Fi'] },
-    { title: 'The Garfield Show', image: '../../../../assets/poster-eleven.jpg', description: 'Cartoon show', actors: ['Frank Welker'], directors: ['Phil Roman'], genres: ['Comedy', 'Family'] },
-    { title: 'Blazing Saddles', image: '../../../../assets/poster-twelve.png', description: 'Western parody', actors: ['Cleavon Little'], directors: ['Mel Brooks'], genres: ['Comedy', 'Western'] },
-    // Add more movies as needed
-  ];
+  filteredMovies: any[] = [];
 
-  filteredMovies = this.movies;
+  constructor(private movieService: MovieService) { }
 
   searchMovies() {
-    this.filteredMovies = this.movies.filter(movie => {
-      return (
-        movie.title.toLowerCase().includes(this.searchCriteria.title.toLowerCase()) &&
-        movie.description.toLowerCase().includes(this.searchCriteria.description.toLowerCase()) &&
-        movie.actors.some(actor => actor.toLowerCase().includes(this.searchCriteria.actors.toLowerCase())) &&
-        movie.directors.some(director => director.toLowerCase().includes(this.searchCriteria.directors.toLowerCase())) &&
-        (this.searchCriteria.genres.length === 0 || this.searchCriteria.genres.some(genre => movie.genres.includes(genre)))
-      );
-    });
+    this.movieService.searchMovies(this.searchCriteria).subscribe(
+      (movies: any[]) => {
+        this.filteredMovies = movies;
+      },
+      error => {
+        console.error('Error searching movies:', error);
+      }
+    );
   }
 }
