b7499593e31caf31e5cfcec9de54e3a7fffccf7c
Vesna Vasic <vasic.vesna.06@gmail.com>
2024-07-06 15:55:43 +0200

    Completed add tv show

--- cloudMovie/src/app/app/layout/add-new-tv-show/add-new-tv-show.component.ts
+++ cloudMovie/src/app/app/layout/add-new-tv-show/add-new-tv-show.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Movie} from "../movie";
 import {MovieService} from "../movie.service";
 import Decimal from "decimal.js";
+import {Director} from "../director.enum";
 
 @Component({
   selector: 'app-add-new-tv-show',
@@ -21,19 +22,27 @@ export class AddNewTvShowComponent {
     description: '',
     numberOfSeasons: "",
     actors: [],
-    director: "Tom Nolan",
+    director: "",
     genres: [],
     duration: new Decimal(0).toString(),
   };
+  actorsInput: string = ""; // Variable to store the input for actors
 
   genres = ['Action', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Romance', 'Sci-Fi', 'Thriller'];
   actors = ['Actor 1', 'Actor 2', 'Actor 3', 'Actor 4'];
-  directors = ['Director 1', 'Director 2', 'Director 3', 'Director 4'];
+  directors = Object.values(Director); // Get the list of directors from the enum
 
   constructor(private movieService: MovieService)  {
   }
 
   onSubmit() {
+        this.show.actors = this.actorsInput.split(',').map(actor => actor.trim().toLowerCase());
+    // Convert title, description, director, and genres to lowercase
+    this.show.title = this.show.title.toLowerCase();
+    this.show.description = this.show.description.toLowerCase();
+    this.show.director = this.show.director.toLowerCase();
+    this.show.genres = this.show.genres.map(genre => genre.toLowerCase());
+
     console.log('Show data:', this.show);
     this.show.numberOfSeasons = new Decimal(this.show.numberOfSeasons).toString()
     this.movieService.uploadMovie(this.show,"")
